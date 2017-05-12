b46162e306229095cf23648cf39f81e557e897d9
Mario Blažek <blazek.mario@gmail.com>
2017-05-12 16:25:24 +0200

    AirPollution and UltravioletIndex tests

--- tests/Core/Weather/AirPollutionTest.php
+++ tests/Core/Weather/AirPollutionTest.php
@@ -2,8 +2,15 @@
 
 namespace Marek\OpenWeatherMap\Tests\Core\Weather;
 
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\DateTime;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\GeographicCoordinates;
+use Marek\OpenWeatherMap\API\Value\Parameter\InputParameterBag;
+use Marek\OpenWeatherMap\API\Value\Response\AirPollution\NitrogenDioxide;
+use Marek\OpenWeatherMap\API\Value\Response\AirPollution\Ozone;
+use Marek\OpenWeatherMap\API\Value\Response\AirPollution\SulfurDioxide;
 use Marek\OpenWeatherMap\API\Weather\Services\AirPollutionInterface;
 use Marek\OpenWeatherMap\Core\Weather\AirPollution;
+use Marek\OpenWeatherMap\Http\Response\JsonResponse;
 
 class AirPollutionTest extends WeatherBase
 {
@@ -22,5 +29,165 @@ class AirPollutionTest extends WeatherBase
     public function testInstanceOfAirPollutionIndex()
     {
         $this->assertInstanceOf(AirPollutionInterface::class, $this->service);
+    }
+
+    public function testFetchOzoneData()
+    {
+        $coords = new GeographicCoordinates(10, 15);
+        $dateTime = new DateTime();
+        $parameterBag = new InputParameterBag(AirPollutionInterface::URL_OZONE);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new Ozone();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(AirPollutionInterface::URL_OZONE)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchOzoneData($coords, $dateTime);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    public function testFetchSulfurDioxideData()
+    {
+        $coords = new GeographicCoordinates(10, 15);
+        $dateTime = new DateTime();
+        $parameterBag = new InputParameterBag(AirPollutionInterface::URL_SULFUR_DIOXIDE);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new SulfurDioxide();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(AirPollutionInterface::URL_SULFUR_DIOXIDE)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchSulfurDioxideData($coords, $dateTime);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    public function testFetchCarbonMonoxideData()
+    {
+        $coords = new GeographicCoordinates(10, 15);
+        $dateTime = new DateTime();
+        $parameterBag = new InputParameterBag(AirPollutionInterface::URL_CARBON_MONOXIDE);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new Ozone();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(AirPollutionInterface::URL_CARBON_MONOXIDE)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchCarbonMonoxideData($coords, $dateTime);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    public function testFetchNitrogenDioxideData()
+    {
+        $coords = new GeographicCoordinates(10, 15);
+        $dateTime = new DateTime();
+        $parameterBag = new InputParameterBag(AirPollutionInterface::URL_NITROGEN_DIOXIDE);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new NitrogenDioxide();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(AirPollutionInterface::URL_NITROGEN_DIOXIDE)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchNitrogenDioxideData($coords, $dateTime);
+
+        $this->assertSame($endResponse, $result);
+
     }
 }
--- tests/Core/Weather/UltravioletIndexTest.php
+++ tests/Core/Weather/UltravioletIndexTest.php
@@ -2,7 +2,11 @@
 
 namespace Marek\OpenWeatherMap\Tests\Core\Weather;
 
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\DateTime;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\GeographicCoordinates;
+use Marek\OpenWeatherMap\API\Value\Parameter\InputParameterBag;
 use Marek\OpenWeatherMap\Core\Weather\UltravioletIndex;
+use Marek\OpenWeatherMap\Http\Response\JsonResponse;
 
 class UltravioletIndexTest extends WeatherBase
 {
@@ -21,5 +25,202 @@ class UltravioletIndexTest extends WeatherBase
     public function testInstanceOfUltravioletIndex()
     {
         $this->assertInstanceOf(UltravioletIndex::class, $this->service);
+    }
+
+    public function testFetchUltravioletIndex()
+    {
+        $coords = new GeographicCoordinates(10, 15);
+        $dateTime = new DateTime();
+        $parameterBag = new InputParameterBag(UltravioletIndex::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new \Marek\OpenWeatherMap\API\Value\Response\UltravioletIndex\UltravioletIndex();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(UltravioletIndex::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchUltravioletIndex($coords, $dateTime);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    public function testFetchUltravioletIndexFromCache()
+    {
+        $coords = new GeographicCoordinates(10, 15);
+        $dateTime = new DateTime();
+        $parameterBag = new InputParameterBag(UltravioletIndex::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new \Marek\OpenWeatherMap\API\Value\Response\UltravioletIndex\UltravioletIndex();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(UltravioletIndex::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(true);
+
+        $this->handler->expects($this->once())
+            ->method('get')
+            ->with($urlHash)
+            ->willReturn('some_data');
+
+        $this->client->expects($this->never())
+            ->method('get');
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchUltravioletIndex($coords, $dateTime);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    /**
+     * @expectedException \Marek\OpenWeatherMap\API\Exception\NotFoundException
+     */
+    public function testFetchUltravioletIndexWithNotFound()
+    {
+        $coords = new GeographicCoordinates(10, 15);
+        $dateTime = new DateTime();
+        $parameterBag = new InputParameterBag(UltravioletIndex::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 404);
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(UltravioletIndex::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->never())
+            ->method('hydrate');
+
+        $this->service->fetchUltravioletIndex($coords, $dateTime);
+    }
+
+    /**
+     * @expectedException \Marek\OpenWeatherMap\API\Exception\BadRequestException
+     */
+    public function testFetchUltravioletIndexWithBadRequest()
+    {
+        $coords = new GeographicCoordinates(10, 15);
+        $dateTime = new DateTime();
+        $parameterBag = new InputParameterBag(UltravioletIndex::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 400);
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(UltravioletIndex::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->never())
+            ->method('hydrate');
+
+        $this->service->fetchUltravioletIndex($coords, $dateTime);
+    }
+
+    /**
+     * @expectedException \Marek\OpenWeatherMap\API\Exception\ForbiddenException
+     */
+    public function testFetchUltravioletIndexWithForbidden()
+    {
+        $coords = new GeographicCoordinates(10, 15);
+        $dateTime = new DateTime();
+        $parameterBag = new InputParameterBag(UltravioletIndex::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 401);
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(UltravioletIndex::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->never())
+            ->method('hydrate');
+
+        $this->service->fetchUltravioletIndex($coords, $dateTime);
     }
 }
