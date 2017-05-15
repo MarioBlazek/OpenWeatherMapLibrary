b8a83657604d31c4cbe266692851b3747ab31866
Mario Blažek <blazek.mario@gmail.com>
2017-05-15 09:09:58 +0200

    Daily and Hour forecasts tests

--- tests/Core/Weather/DailyForecastTest.php
+++ tests/Core/Weather/DailyForecastTest.php
@@ -2,8 +2,17 @@
 
 namespace Marek\OpenWeatherMap\Tests\Core\Weather;
 
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\CityId;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\CityName;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\DaysCount;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\Latitude;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\Longitude;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\ZipCode;
+use Marek\OpenWeatherMap\API\Value\Parameter\InputParameterBag;
+use Marek\OpenWeatherMap\API\Value\Response\DailyForecast\AggregatedDailyForecast;
 use Marek\OpenWeatherMap\API\Weather\Services\DailyForecastInterface;
 use Marek\OpenWeatherMap\Core\Weather\DailyForecast;
+use Marek\OpenWeatherMap\Http\Response\JsonResponse;
 
 class DailyForecastTest extends WeatherBase
 {
@@ -22,5 +31,165 @@ class DailyForecastTest extends WeatherBase
     public function testInstanceOfDailyForecastInterface()
     {
         $this->assertInstanceOf(DailyForecastInterface::class, $this->service);
+    }
+
+    public function testFetchForecastByCityName()
+    {
+        $cityName = new CityName('Zagreb');
+        $numberOfDays = new DaysCount(1);
+        $parameterBag = new InputParameterBag(DailyForecastInterface::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new AggregatedDailyForecast();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(DailyForecastInterface::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchForecastByCityName($cityName, $numberOfDays);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    public function testFetchForecastByCityId()
+    {
+        $cityId = new CityId(12345);
+        $numberOfDays = new DaysCount(1);
+        $parameterBag = new InputParameterBag(DailyForecastInterface::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new AggregatedDailyForecast();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(DailyForecastInterface::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchForecastByCityId($cityId, $numberOfDays);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    public function testFetchForecastByZipCode()
+    {
+        $zipCode = new ZipCode(12345);
+        $numberOfDays = new DaysCount(1);
+        $parameterBag = new InputParameterBag(DailyForecastInterface::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new AggregatedDailyForecast();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(DailyForecastInterface::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchForecastByZipCode($zipCode, $numberOfDays);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    public function testFetchForecastByGeographicCoordinates()
+    {
+        $latitude = new Latitude(10);
+        $longitude = new Longitude(15);
+        $numberOfDays = new DaysCount(1);
+        $parameterBag = new InputParameterBag(DailyForecastInterface::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new AggregatedDailyForecast();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(DailyForecastInterface::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchForecastByCityGeographicCoordinates($latitude, $longitude, $numberOfDays);
+
+        $this->assertSame($endResponse, $result);
     }
 }
--- tests/Core/Weather/HourForecastTest.php
+++ tests/Core/Weather/HourForecastTest.php
@@ -2,8 +2,16 @@
 
 namespace Marek\OpenWeatherMap\Tests\Core\Weather;
 
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\CityId;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\CityName;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\Latitude;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\Longitude;
+use Marek\OpenWeatherMap\API\Value\Parameter\Input\ZipCode;
+use Marek\OpenWeatherMap\API\Value\Parameter\InputParameterBag;
+use Marek\OpenWeatherMap\API\Value\Response\HourForecast\AggregatedHourForecast;
 use Marek\OpenWeatherMap\API\Weather\Services\HourForecastInterface;
 use Marek\OpenWeatherMap\Core\Weather\HourForecast;
+use Marek\OpenWeatherMap\Http\Response\JsonResponse;
 
 class HourForecastTest extends WeatherBase
 {
@@ -22,5 +30,162 @@ class HourForecastTest extends WeatherBase
     public function testInstanceOfHourForecastInterface()
     {
         $this->assertInstanceOf(HourForecastInterface::class, $this->service);
+    }
+
+    public function testFetchForecastByCityName()
+    {
+        $cityName = new CityName('Zagreb');
+        $parameterBag = new InputParameterBag(HourForecastInterface::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new AggregatedHourForecast();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(HourForecastInterface::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchForecastByCityName($cityName);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    public function testFetchForecastByCityId()
+    {
+        $cityId = new CityId(12345);
+        $parameterBag = new InputParameterBag(HourForecastInterface::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new AggregatedHourForecast();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(HourForecastInterface::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchForecastByCityId($cityId);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    public function testFetchForecastByZipCode()
+    {
+        $zipCode = new ZipCode(12345);
+        $parameterBag = new InputParameterBag(HourForecastInterface::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new AggregatedHourForecast();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(HourForecastInterface::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchForecastByZipCode($zipCode);
+
+        $this->assertSame($endResponse, $result);
+
+    }
+
+    public function testFetchForecastByGeographicCoordinates()
+    {
+        $latitude = new Latitude(10);
+        $longitude = new Longitude(15);
+        $parameterBag = new InputParameterBag(HourForecastInterface::BASE_URL);
+        $url = 'url';
+        $urlHash = md5($url);
+        $response = new JsonResponse(['data' => 'data'], 200);
+        $endResponse = new AggregatedHourForecast();
+
+        $this->factory->expects($this->once())
+            ->method('buildBag')
+            ->with(HourForecastInterface::BASE_URL)
+            ->willReturn($parameterBag);
+
+        $this->factory->expects($this->once())
+            ->method('build')
+            ->with($parameterBag)
+            ->willReturn($url);
+
+        $this->handler->expects($this->once())
+            ->method('has')
+            ->with($urlHash)
+            ->willReturn(false);
+
+        $this->client->expects($this->once())
+            ->method('get')
+            ->with($url)
+            ->willReturn($response);
+
+        $this->hydrator->expects($this->once())
+            ->method('hydrate')
+            ->willReturn($endResponse);
+
+        $result = $this->service->fetchForecastByCityGeographicCoordinates($latitude, $longitude);
+
+        $this->assertSame($endResponse, $result);
+
     }
 }
