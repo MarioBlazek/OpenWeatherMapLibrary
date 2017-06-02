f4a0005c6bffa2849a61cfaf4318b99ec201ca80
Mario Blažek <blazek.mario@gmail.com>
2017-06-02 09:58:41 +0200

    ForecastHydrators tests

--- lib/API/Value/Response/HourForecast/HourForecast.php
+++ lib/API/Value/Response/HourForecast/HourForecast.php
@@ -2,7 +2,12 @@
 
 namespace Marek\OpenWeatherMap\API\Value\Response\HourForecast;
 
+use Marek\OpenWeatherMap\API\Value\Response\Wind;
+
 class HourForecast
 {
-
+    /**
+     * @var Wind
+     */
+    public $wind;
 }
